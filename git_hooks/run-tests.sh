56cf6ed396f4f0c06785c0e7314a757cfa741b05
Globegitter <markus.padourek@gmail.com>
2013-10-20 19:24:10 -0400

    Renamed modulesTest to moduleTests

--- git_hooks/run-tests.sh
+++ git_hooks/run-tests.sh
@@ -1,1 +1,1 @@
-matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(modulesTest); disp(result); for i=1:length(result) if (result(i).Failed > 0) exit(1); end; end; exit(0);"
+matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(moduleTests); disp(result); for i=1:length(result) if (result(i).Failed > 0) exit(1); end; end; exit(0);"
