58c96ec392f0bd375ff6cf0c5b59fab3c53eed0a
Globegitter <markus.padourek@gmail.com>
2013-10-20 17:53:22 -0400

    Testing git pre-commit hook. Again.

--- git_hooks/run_tests.sh
+++ git_hooks/run_tests.sh
@@ -1,1 +1,1 @@
-matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(modulesTest); disp(result); disp(result(0).Failed); if (result.Failed > 0) exit(1); else exit(0); end"
+matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(modulesTest); disp(result); disp(result(1).Failed); if (result.Failed > 0) exit(1); else exit(0); end"
