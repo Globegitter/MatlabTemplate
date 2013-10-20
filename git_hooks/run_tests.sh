37b1fe1f47e70170a500630ed361dbfb32f88412
Globegitter <markus.padourek@gmail.com>
2013-10-20 16:47:27 -0400

    Added second testfunction

--- git_hooks/run_tests.sh
+++ git_hooks/run_tests.sh
@@ -1,1 +1,1 @@
-matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(basicTest); if result.Failed > 0 exit(1); else exit(0); end"
+matlab -nosplash -nodesktop –nojvm –noFigureWindows -nodisplay -r "result=run(moduleTests); if result.Failed > 0 exit(1); else exit(0); end"
