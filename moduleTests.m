239edfd9cf9029b7465d082280cf4003cb8f36c7
Globegitter <markus.padourek@gmail.com>
2013-10-21 14:10:14 -0400

    Added missing moduleTests file

--- moduleTests.m
+++ moduleTests.m
@@ -21,15 +21,15 @@ classdef moduleTests < matlab.unittest.TestCase
     end
 
     methods(Test)
-        %Naming: e.g. testCoordinateAscent or testADMM, whatever module you are testing
+        %Naming: e.g. testCoordinateAscent or testDictionaryUpdate; whatever module you are testing
         %or you can get a bit more specific, e.g. testDimensions or testADMMDimensions
         
         function testName(testCase)
-            %Initialize some testcalues and run the function that is being tested
-            %then use one of the following functions
+            %Initialize some testvalues and run the function that is being tested as you normally would do
+            %then use one of the following functions:
             %testCase.assertSize(matrix, size) e.g. assertSize(A, [10 2]);
             %testCase.assertEqual(actualSoltion, expectedSolution, 'AbsTol', 0.01)
-            %For a complete list see
+            %For a complete list see:
             %http://www.mathworks.com/help/matlab/matlab_prog/select-qualifications.html
         end
 
